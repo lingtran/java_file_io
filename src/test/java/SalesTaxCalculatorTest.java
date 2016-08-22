592995bd23384e22dac5ca2caef51860705d2f7c
lingtran <lxiutran@gmail.com>
2016-08-22 16:37:14 -0600

    pass SalesTaxCalculator tests

---
+++ src/test/java/SalesTaxCalculatorTest.java
@@ -0,0 +1,158 @@
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SalesTaxCalculatorTest {
+
+    @Test
+    public void testCalculate() {
+        ShoppingBasket test 	= new ShoppingBasket();
+        Item itemOne	 		= new Item( "1", "imported box of chocolates", "11.25" );
+        Item itemTwo	 		= new Item( "1", "perfume", "10.79" );
+        Double itemOneSalesTax	= 0.6;
+        Double itemOneTotal		= 11.85;
+        Double itemTwoSalesTax	= 1.1;
+        Double itemTwoTotal		= 11.889999999999999;
+        Double basketSalesTaxes = 1.7;
+        Double basketTotal		= itemOneTotal + itemTwoTotal;
+
+        test.basketOfItems.add(itemOne);
+        test.basketOfItems.add(itemTwo);
+        SalesTaxCalculator.calculate(test);
+
+        assertEquals( itemOneSalesTax, itemOne.salesTax );
+        assertEquals(  itemOneTotal, itemOne.total );
+        assertEquals (itemTwoSalesTax, itemTwo.salesTax );
+        assertEquals( itemTwoTotal, itemTwo.total );
+        assertEquals( basketSalesTaxes, test.salesTaxes );
+        assertEquals( basketTotal, test.total );
+    }
+
+    @Test
+    public void testAddToBasketSalesTaxes() {
+        ShoppingBasket test = new ShoppingBasket();
+        Item itemOne	 	= new Item( "1", "imported box of chocolates", "11.25" );
+        Double salesTaxes   = 0.6;
+        Double total        = 11.85;
+
+        test.basketOfItems.add(itemOne);
+
+        SalesTaxCalculator.determineItemBasicSalesTax(itemOne);
+        SalesTaxCalculator.determineItemImportDuty(itemOne);
+        SalesTaxCalculator.sumTotalFor(itemOne);
+        SalesTaxCalculator.addToBasketSalesTaxes(test, itemOne.salesTax);
+        SalesTaxCalculator.addToBasketTotal(test, itemOne.total);
+
+        assertEquals( salesTaxes, test.salesTaxes );
+        assertEquals( total, test.total );
+    }
+
+    @Test
+    public void testAddToBasketTotal() {
+        ShoppingBasket test = new ShoppingBasket();
+        Item itemOne	 	= new Item( "1", "perfume", "10.79" );
+        Double total        = 11.889999999999999;
+
+        test.basketOfItems.add(itemOne);
+
+        SalesTaxCalculator.determineItemBasicSalesTax(itemOne);
+        SalesTaxCalculator.determineItemImportDuty(itemOne);
+        SalesTaxCalculator.sumTotalFor(itemOne);
+        SalesTaxCalculator.addToBasketTotal(test, itemOne.total);
+
+        assertEquals( total, test.total );
+    }
+
+    @Test
+    public void testSumTotalFor() {
+        Item test	 	= new Item( "1", "perfume", "10.79" );
+        Double salesTax = 1.1;
+        Double total    = 11.889999999999999;
+
+        SalesTaxCalculator.determineItemBasicSalesTax(test);
+        SalesTaxCalculator.determineItemImportDuty(test);
+        SalesTaxCalculator.sumTotalFor(test);
+
+        assertEquals( salesTax, test.salesTax );
+        assertEquals( total, test.total );
+    }
+
+    @Test
+    public void testDetermineItemBasicSalesTax() {
+        Item testOne 		   = new Item( "1", "perfume", "10.79" );
+        Double testOneSalesTax = 1.1;
+
+        testOne.determineIfExempt();
+        SalesTaxCalculator.determineItemBasicSalesTax(testOne);
+
+        assertFalse( testOne.exemptionStatus);
+        assertEquals( testOneSalesTax, testOne.salesTax);
+
+        Item testTwo		   = new Item( "1", "chocolate", "10.00" );
+        Double testTwoSalesTax = 0.00;
+
+        testTwo.determineIfExempt();
+
+        assertTrue(  testTwo.exemptionStatus );
+
+        SalesTaxCalculator.determineItemBasicSalesTax(testTwo);
+
+        assertEquals( testTwoSalesTax, testTwo.salesTax );
+
+    }
+
+    @Test
+    public void testDetermineItemImportDuty() {
+        Item testOne			    = new Item( "1", "box of books", "27.99" );
+        Double testOneBasicSalesTax = 0.0;
+        Double testOneImportDuty 	= 0.0;
+        Double testOneSalesTax 		= testOneBasicSalesTax + testOneImportDuty;
+
+        SalesTaxCalculator.determineItemBasicSalesTax(testOne);
+        SalesTaxCalculator.determineItemImportDuty(testOne);
+
+        assertTrue( testOne.exemptionStatus );
+        assertFalse( testOne.importStatus );
+        assertEquals( testOneBasicSalesTax, testOne.salesTax );
+        assertEquals( testOneImportDuty, testOne.salesTax );
+        assertEquals( testOneSalesTax, testOne.salesTax );
+
+        Item testTwo				= new Item( "1", "imported bottle of perfume", "18.99" );
+        Double testTwoSalesTax	    = 2.85;
+
+        SalesTaxCalculator.determineItemBasicSalesTax(testTwo);
+        SalesTaxCalculator.determineItemImportDuty(testTwo);
+
+        assertFalse( testTwo.exemptionStatus );
+        assertTrue( testTwo.importStatus );
+        assertEquals( testTwoSalesTax, testTwo.salesTax);
+    }
+
+    @Test
+    public void testRoundUpSalesTax() {
+        Double taxOne		 = 7.61;
+        Double roundedTaxOne = 7.65;
+        Double taxTwo		 = 6.50;
+        Double roundedTaxTwo = 6.50;
+
+        Double testOne = SalesTaxCalculator.roundUpSalesTax(taxOne);
+        Double testTwo = SalesTaxCalculator.roundUpSalesTax(taxTwo);
+
+        assertEquals( roundedTaxOne, testOne );
+        assertEquals( roundedTaxTwo, testTwo );
+
+        Item importedChoco	 		  = new Item("1", "imported box of chocolates", "10.00");
+        Item importedPerfume		  = new Item("1", "imported bottle of perfume", "47.50");
+        Double roundedChocoSalesTax	  = 0.5;
+        Double roundedPerfumeSalesTax = 7.15;
+
+        SalesTaxCalculator.determineItemBasicSalesTax(importedChoco);
+        SalesTaxCalculator.determineItemImportDuty(importedChoco);
+        SalesTaxCalculator.determineItemBasicSalesTax(importedPerfume);
+        SalesTaxCalculator.determineItemImportDuty(importedPerfume);
+
+        assertEquals( roundedChocoSalesTax, importedChoco.salesTax);
+        assertEquals( roundedPerfumeSalesTax, importedPerfume.salesTax);
+    }
+}
